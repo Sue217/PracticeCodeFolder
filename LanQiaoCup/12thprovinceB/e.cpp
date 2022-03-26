c1e790dbc923aa2a9bdc767eff50e49ee1e93114
Sudo <sujingbo02@126.com>
2022-03-26 22:35:12 +0800

    2022/03/26

--- LanQiaoCup/12thprovinceB/e.cpp
+++ LanQiaoCup/12thprovinceB/e.cpp
@@ -18,44 +18,32 @@ int main() {
   int n, m, k;
   cin >> n >> m >> k;
   const long long mod = 1000000007;
-  array<int, 8> dx{-2, -2, -1, -1, 1, 1, 2, 2}, dy{-1, 1, -2, 2, -2, 2, -1, 1};
-  vector<vector<int>> hack(n, vector<int>(m));
-  function<long long(int, int, int)> Dfs = [&](int x, int y, int z)->long long {
-    if (z == k) {
-      return 1;
-    }
-    long long ans = 0;
-    for (int i = 0; i < 8; i++) {
-      int xx = x + dx[i];
-      int yy = y + dy[i];
-      if (xx >= 0 && xx < n && yy >= 0 && yy < m && !hack[xx][yy]) {
-        hack[xx][yy] = true;
-        ans = (ans + Dfs(xx, yy, z + 1)) % mod;
-        hack[xx][yy] = false;
-      }
-    }
-    return ans;
+  const int T = 1 << n;
+  vector dp(m + 1, vector<vector<vector<long long>>>(T, vector<vector<long long>>(T, vector<long long>(k + 1))));
+  dp[0][0][0][0] = 1;
+  auto check = [&](int x, int y, int z) {
+    return !(x << (3 - z) & y || y << (3 - z) & x);
   };
-  int N = n * m;
-  vector<vector<long long>> C(N + 1, vector<long long>(k + 1));
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= k; j++) {
-      if (i == j) {
-        C[i][j] = 1;
-        continue;
+  for (int i = 1; i <= m; i++) {
+    for (int x = 0; x < T; x++) {
+      for (int y = 0; y < T; y++) {
+        if (check(x, y, 1)) {
+          for (int z = 0; z < T; z++) {
+            if (check(x, z, 2) && check(y, z, 1)) {
+              int one = __builtin_popcount(z);
+              for (int t = k; t >= one; t--) {
+                dp[i][y][z][t] = (dp[i][y][z][t] + dp[i - 1][x][y][t - one]) % mod;
+              }
+            }
+          }
+        }
       }
-      if (j == 1) {
-        C[i][j] = i;
-        continue;
-      }
-      C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
     }
   }
-  C[N][k] %= mod;
   long long ans = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      ans = (ans + Dfs(i, j, 1)) % mod;
+  for (int x = 0; x < T; x++) {
+    for (int y = 0; y < T; y++) {
+      ans = (ans + dp[m][x][y][k]) % mod;
     }
   }
   cout << ans << '\n';
